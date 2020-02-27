6faa0a4661e8d76710bc986d0bf307ad65faa00f
Thomas Sears <0tsears0@gmail.com>
2020-02-27 17:16:11 -0500

    added parameters to tables: department, employee_role, employee

--- db/schema.sql
+++ db/schema.sql
@@ -2,14 +2,25 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE employee (
-
-);
-
-CREATE TABLE role (
-
-);
-
 CREATE TABLE department (
+name VARCHAR(30),
+id INT NOT NULL,
+PRIMARY KEY (id)
+);
 
+CREATE TABLE employee_role (
+id INT NOT NULL,
+PRIMARY KEY (id),
+title VARCHAR(30),
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+id INT NOT NULL,
+PRIMARY KEY (id),
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT NOT NULL,
+manager_id INT
 );
