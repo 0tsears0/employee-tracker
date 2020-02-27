36d88933eaf52aa2df537c963953e1aa27fb541d
Thomas Sears <0tsears0@gmail.com>
2020-02-27 17:05:17 -0500

    created employee_db and added empty tables: employee, role, department

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employee (
+
+);
+
+CREATE TABLE role (
+
+);
+
+CREATE TABLE department (
+
+);
